1b7540a288da98b17c833e978ece7d8b4f723da8
Soham Maji <88835836+mafiariders7@users.noreply.github.com>
2022-12-29 21:50:08 +0530

    .

--- main.cpp
+++ main.cpp
@@ -1,21 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void quickSort(int arr[], int low, int high)
-{
-  if (low < high)
-  {
 
-    int pi = partition(arr, low, high);
-
-    quickSort(arr, low, pi - 1);
-    quickSort(arr, pi + 1, high);
-  }
-}
 
 int main()
 {
-
-  cout << "hello world!!";
-  cout << "hi boys";
+ int n = 10;
+ 
 }
